009ec0d2a6023e4efb1e668e73f8838f58d14f05
ankur <ankurj630@gmail.com>
2018-04-12 22:22:53 +0530

    base parameters defined in config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+# ----------------------  BASE PARAMETERS ---------------------- #
 DOCKER_IMAGE = 'ankur/python'
+LOCAL_DIR = '/home/ankur/Documents/Projects/Docker/compiler/temp'
+
+# ----------------------  ADDITIONAL PARAMETERS ---------------------- #
 MEMORY_LIMIT = '8000k'
 AUTO_REMOVE = True
 FILE_OPEN_MODE = 'ro' #read only : use rw for read write
-LOCAL_DIR = '/home/ankur/Documents/Projects/Docker/compiler/temp'
 CONTAINER_DIR = '/data'
 CONTAINER_TIMEOUT = 8
