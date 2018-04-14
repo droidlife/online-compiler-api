ea4b39842be755d1cfe056e9f948f43f19f99daf
ankur <ankurj630@gmail.com>
2018-04-14 22:57:13 +0530

    refactored

--- compile/util/java_compiler.py
+++ compile/util/java_compiler.py
@@ -1,48 +1,45 @@
 import docker
 import os
-from config import DOCKER_IMAGE, MEMORY_LIMIT, AUTO_REMOVE, FILE_OPEN_MODE, LOCAL_DIR, CONTAINER_DIR
-import subprocess
+from config import DOCKER_IMAGE, LOCAL_DIR, CONTAINER_DIR
 import traceback
+
 
 def java_runner(client, file_name, container_name, command_string, return_dict):
     try:
         local_directory = LOCAL_DIR + '/' + str(file_name)
-        
+
         if not os.path.exists(local_directory):
             raise Exception('File not found : ' + str(local_directory))
 
         with open(local_directory, 'r') as content_file:
             content = content_file.read()
-        
+
         try:
             void_main_splitter = content.split("public static void main")
             class_splitter = void_main_splitter[0].split("class")
-            psvm_class_name = class_splitter[len(class_splitter)-1].split('{')[0].strip()
+            psvm_class_name = class_splitter[len(class_splitter) - 1].split('{')[0].strip()
 
             print psvm_class_name
         except:
             return_dict['result'] = "public static void main not found!"
             return
-        
+
         psvm_java_file_name = str(psvm_class_name) + '.java'
         container_directory = CONTAINER_DIR + '/' + psvm_java_file_name
 
         java_output_dir = LOCAL_DIR + '/' + container_name + 'temp'
-        f= open(java_output_dir,"w+")
-        f.close()
+        _file = open(java_output_dir, "w+")
+        _file.close()
 
-        container_run_command = 'docker run -d -it -v {0}:{1} --name {2} {3}'.format(local_directory, container_directory, 
-                                                            container_name, DOCKER_IMAGE) 
-        print container_run_command
-        
+        container_run_command = 'docker run -d -it -v {0}:{1} --name {2} {3}'.format(local_directory,
+                                                                                     container_directory,
+                                                                                     container_name, DOCKER_IMAGE)
         os.system(container_run_command)
 
         container_id = client.containers.list(filters={'name': container_name})[0].short_id
 
-        java_compile_command = 'docker exec -it {0} javac {1}'.format(container_id, 
-                                                                     str(psvm_java_file_name))
+        java_compile_command = 'docker exec -it {0} javac {1}'.format(container_id, str(psvm_java_file_name))
         system_compile_command = '{0} > {1}'.format(java_compile_command, java_output_dir)
-        print java_compile_command
         os.system(system_compile_command)
 
         with open(java_output_dir, 'r') as content_file:
@@ -52,10 +49,8 @@ def java_runner(client, file_name, container_name, command_string, return_dict):
             os.remove(java_output_dir)
             return_dict['result'] = str(compile_output)
             return
-        
-        java_run_command = 'docker exec -it {0} java {1}'.format(container_id, 
-                                                                str(psvm_class_name))
-        print java_run_command
+
+        java_run_command = 'docker exec -it {0} java {1}'.format(container_id, str(psvm_class_name))
         system_run_command = '{0} > {1}'.format(java_run_command, java_output_dir)
         os.system(system_run_command)
 
@@ -63,8 +58,6 @@ def java_runner(client, file_name, container_name, command_string, return_dict):
 
         os.remove(java_output_dir)
         return_dict['result'] = str(run_output)
-    except docker.errors.ContainerError as e:
-        return_dict['result'] = e.message
     except Exception as e:
         print traceback.print_exc()
         return_dict['result'] = e.message
--- compile/util/runner.py
+++ compile/util/runner.py
@@ -5,6 +5,7 @@ import multiprocessing
 from compile.service import get_command, get_extension, get_target_method
 from config import LOCAL_DIR, CONTAINER_TIMEOUT
 
+
 def run_code(code, language, version):
     # getting the docker client
     client = docker.from_env()
@@ -15,11 +16,11 @@ def run_code(code, language, version):
 
     # getting the command to be run
     result, command_string = get_command(language, version)
-    
+
     if not result:
-        return_dict['result'] = command_string 
+        return_dict['result'] = command_string
         return return_dict
-    
+
     # saving the code in a temp file 
     container_name = str(hashlib.sha1(os.urandom(128)).hexdigest())[:10]
     file_name = container_name + str(get_extension(language))
@@ -28,13 +29,12 @@ def run_code(code, language, version):
     file.write(code)
     file.close()
 
-    
     # getting which compiler method will handle this request
     target_method = get_target_method(language)
 
     # starting the docker container in separate process
-    process = multiprocessing.Process(target=target_method, 
-                                    args=(client, file_name, container_name, command_string, return_dict))
+    process = multiprocessing.Process(target=target_method,
+                                      args=(client, file_name, container_name, command_string, return_dict))
     process.start()
 
     # setting the timeout for the process
@@ -45,7 +45,7 @@ def run_code(code, language, version):
         process.terminate()
         process.join()
         return_dict['result'] = 'Time limit Exceeded'
-    
+
     # check if the container is still running. if yes, kill the container
     running_containers = client.containers.list(filters={'name': container_name}, all=True)
     if running_containers:
@@ -54,4 +54,3 @@ def run_code(code, language, version):
     # remove the temp file
     os.remove(file_path)
     return return_dict
-
